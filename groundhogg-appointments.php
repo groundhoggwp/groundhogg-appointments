aa2b797e5a987e17d0bf22a58d69ae14c0b45442
tobeyadr <adrian@trainingbusinesspros.com>
2020-05-07 09:11:44 -0400

    update requirment to 2.2.aplha.1

--- groundhogg-appointments.php
+++ groundhogg-appointments.php
@@ -36,48 +36,113 @@ define( 'GROUNDHOGG_BOOKING_CALENDAR_ASSETS_PATH', GROUNDHOGG_BOOKING_CALENDAR_P
 define( 'GROUNDHOGG_BOOKING_CALENDAR_ASSETS_URL', GROUNDHOGG_BOOKING_CALENDAR_URL . 'assets/' );
 define( 'GROUNDHOGG_BOOKING_CALENDAR_ZOOM_BASE_URL','https://api.zoom.us/v2/' );
 
+define( 'GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_WP_VERSION', '4.9' );
+define( 'GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_PHP_VERSION', '7.0' );
+define( 'GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_CORE_VERSION', '2.2.alpha.1' );
+
 add_action( 'plugins_loaded', function (){
     load_plugin_textdomain( GROUNDHOGG_BOOKING_CALENDAR_TEXT_DOMAIN, false, basename( dirname( __FILE__ ) ) . '/languages' );
 } );
 
 define( 'GROUNDHOGG_BOOKING_CALENDAR_TEXT_DOMAIN', 'groundhogg-calendar' );
 
-if ( ! version_compare( PHP_VERSION, '5.6', '>=' ) ) {
-    add_action( 'admin_notices', function(){
-        $message = sprintf( esc_html__( '%s requires PHP version %s+, plugin is currently NOT RUNNING.', 'groundhogg-calendar' ), GROUNDHOGG_BOOKING_CALENDAR_NAME, '5.6' );
-        $html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
-        echo wp_kses_post( $html_message );
-    } );
-} elseif ( ! version_compare( get_bloginfo( 'version' ), '4.9', '>=' ) ) {
-    add_action( 'admin_notices', function (){
-        $message = sprintf( esc_html__( '%s requires WordPress version %s+. Because you are using an earlier version, the plugin is currently NOT RUNNING.', 'groundhogg-calendar' ), GROUNDHOGG_BOOKING_CALENDAR_NAME, '4.9' );
-        $html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
-        echo wp_kses_post( $html_message );
-    } );
-} else {
 
-    // Groundhogg is loaded, load now.
-    if ( did_action( 'groundhogg/loaded' ) ){
+// Check PHP and WP are up to date!
+if ( check_wp_version() && check_php_version() ){
 
-        require GROUNDHOGG_BOOKING_CALENDAR_PATH . 'includes/plugin.php';
+	// Groundhogg is loaded, load now.
+	if ( did_action( 'groundhogg/loaded' ) ) {
 
-        // Lazy load, wait for Groundhogg!
-    } else {
-        add_action('groundhogg/loaded', function () {
-            require GROUNDHOGG_BOOKING_CALENDAR_PATH . 'includes/plugin.php';
-        });
+		if ( check_core_version() ){
+			require __DIR__ . '/includes/plugin.php';
+		}
 
-        // Might not actually be loaded, so we'll check in later.
-        add_action( 'admin_notices', function () {
+		// Lazy load, wait for Groundhogg!
+	} else {
 
-            // Is not loaded!
-            if ( ! defined( 'GROUNDHOGG_VERSION' ) ){
-                $message = sprintf(esc_html__('Groundhoggg is not currently active, it must be active for %s to work.', 'groundhogg-calendar'), GROUNDHOGG_BOOKING_CALENDAR_NAME );
-                $html_message = sprintf('<div class="notice notice-warning">%s</div>', wpautop($message));
-                echo wp_kses_post($html_message);
-            }
-        });
-    }
+		add_action( 'groundhogg/loaded', function () {
+			if ( check_core_version() ){
+				require __DIR__ . '/includes/plugin.php';
+			}
+		} );
+
+		// Might not actually be loaded, so we'll check in later.
+		check_groundhogg_active();
+	}
 }
 
+/**
+ * Check that Gorundhogg is using the latest available core version
+ *
+ * @return bool|int
+ */
+function check_core_version() {
 
+	$correct_version = version_compare( GROUNDHOGG_VERSION, GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_CORE_VERSION, '>=' );
+
+	if ( ! $correct_version ) {
+		add_action( 'admin_notices', function () {
+			$message      = sprintf( esc_html__( '%s requires Groundhogg version %s+. Because you are using an earlier version, the plugin is currently NOT RUNNING.', 'groundhogg' ), GROUNDHOGG_BOOKING_CALENDAR_NAME, GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_CORE_VERSION );
+			$html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
+			echo wp_kses_post( $html_message );
+		} );
+	}
+
+	return $correct_version;
+}
+
+/**
+ * Check that the wp version is most recent
+ *
+ * @return bool|int
+ */
+function check_wp_version() {
+
+	$correct_version = version_compare( get_bloginfo( 'version' ), GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_WP_VERSION, '>=' );
+
+	if ( ! $correct_version ) {
+		add_action( 'admin_notices', function () {
+			$message      = sprintf( esc_html__( '%s requires WordPress version %s+. Because you are using an earlier version, the plugin is currently NOT RUNNING.', 'groundhogg' ), GROUNDHOGG_BOOKING_CALENDAR_NAME, GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_WP_VERSION );
+			$html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
+			echo wp_kses_post( $html_message );
+		} );
+	}
+
+	return $correct_version;
+}
+
+/**
+ * Check that the PHP version is compatible
+ *
+ * @return bool|int
+ */
+function check_php_version() {
+
+	$correct_version = version_compare( PHP_VERSION, GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_PHP_VERSION, '>=' );
+
+	if ( ! $correct_version ) {
+		add_action( 'admin_notices', function () {
+			$message      = sprintf( esc_html__( '%s requires PHP version %s+, plugin is currently NOT RUNNING.', 'groundhogg' ), GROUNDHOGG_BOOKING_CALENDAR_NAME, GROUNDHOGG_BOOKING_CALENDAR_REQUIRED_PHP_VERSION );
+			$html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
+			echo wp_kses_post( $html_message );
+		} );
+	}
+
+	return $correct_version;
+}
+
+/**
+ * Check that Groundhogg is active!
+ */
+function check_groundhogg_active() {
+	// Might not actually be loaded, so we'll check in later.
+	add_action( 'admin_notices', function () {
+
+		// Is not loaded!
+		if ( ! defined( 'GROUNDHOGG_VERSION' ) ) {
+			$message      = sprintf( esc_html__( 'Groundhogg is not currently active, it must be active for %s to work.', 'groundhogg' ), GROUNDHOGG_BOOKING_CALENDAR_NAME );
+			$html_message = sprintf( '<div class="notice notice-warning">%s</div>', wpautop( $message ) );
+			echo wp_kses_post( $html_message );
+		}
+	} );
+}
